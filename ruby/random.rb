33c0e9aeeb0e487d74e5dbfbd8f0a9c083265b61
kiyo <18kyohei@gmail.com>
2024-06-21 13:57:37 +0900

    remove unnecessary code

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,4 +1,4 @@
-groups = ['A', 'B' ,'C' ,'D' ,'E' ,'F']
+groups = ['A', 'B','C', 'D','E','F']
 numbers = [1, 2]
 number =  numbers.sample(1)
 if number == [1]
@@ -9,36 +9,4 @@ elsif number == [2]
   p divides_first = groups.sample(2).sort
   p groups - divides_first
 end
-
-# class Group
-#   groups = ['A', 'B' ,'C' ,'D' ,'E' ,'F']
-#   def divides_even
-#     p divides_first = groups.sample(3).sort
-#     p groups - divides_first
-#   end
-#   def divides_uneven
-#     p divides_first = groups.sample(2).sort
-#     p groups - divides_first
-#   end
-# end
-
-# numbers = [1, 2]
-# number =  numbers.sample(1)
-# group = Group.new
-#  number == [1] ? group.divides_even : group.divides_uneven
-
-
-
-
-# groups = ['A', 'B' ,'C' ,'D' ,'E' ,'F']
-# numbers = [1, 2]
-
-# number =  numbers.sample(1)
-# if number == [1]
-#   p divides_first = groups.sample(3).sort
-#   p groups - divides_first
-
-# elsif number == [2]
-#   p divides_first = groups.sample(2).sort
-#   p groups - divides_first
-# end
+  
