66ff4b494f61c1086780e3646a0ae55e89a51cbe
kiyo <18kyohei@gmail.com>
2024-06-22 16:08:54 +0900

    change divides to divide

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,5 +1,5 @@
 groups = ['A', 'B','C','D','E','F']
 numbers = [2, 3]
 number = numbers.sample
-p divides_first = groups.sample(number).sort
-p groups - divides_first
+p divide_first = groups.sample(number).sort
+p groups - divide_first
