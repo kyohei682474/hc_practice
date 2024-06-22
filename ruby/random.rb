4bc7c90f6ea26ce81c9bbaf61a93ea0c3c8d03a6
kiyo <18kyohei@gmail.com>
2024-06-22 15:57:38 +0900

    change numbers arrary and change divide_first and delete if syntax

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,12 +1,5 @@
 groups = ['A', 'B','C','D','E','F']
-numbers = [1, 2]
-number =  numbers.sample(1)
-if number == [1]
-  p divides_first = groups.sample(3).sort
-  p groups - divides_first
-
-elsif number == [2]
-  p divides_first = groups.sample(2).sort
-  p groups - divides_first
-end
-  
+numbers = [2, 3]
+number = numbers.sample
+p divides_first = groups.sample(number).sort
+p groups - divides_first
