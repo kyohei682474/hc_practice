12a036dc383f4cca2f910d4d4762323d7ce2ee0b
kiyo <18kyohei@gmail.com>
2024-06-26 06:46:47 +0900

    add option month

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,16 +1,28 @@
 require 'optparse'
 require 'date'
+
+# コマンドの解析
 opt = OptionParser.new
-params = {} 
-opt.on('-m [value]'){|v| params[:m] = v }
+options = {}
+opt.on('-m [mon]', Integer) { |mon| options[:m] = mon }
 opt.parse!(ARGV)
 p ARGV
+
+now = Date.today
+year = Date.today.year
+
+mon = options[:m] || now.month
+
+if mon < 1 || 12 < mon
+  puts "#{mon} is neither a month number (1..12) nor a name"
+end
+
 
 weeks = ["日","月","火","水","木","金","土",]
 # %m: 月を表す数字(01-12)　%Y: 西暦を表す数
 top = Date.today.strftime("%m, %Y")
-year = Date.today.year
-mon = Date.today.month
+# year = Date.today.year
+# mon = Date.today.month
 # 初日が何曜日かを把握
 first_wday = Date.new(year, mon, 1).wday
 # 最終日が何日か把握
@@ -21,12 +33,15 @@ puts weeks.join(' ')
 print " " * (first_wday * 3)
 (1..last_date).each do |date|
   print date.to_s.rjust(2) + " " 
-if (date + first_wday) % 7 == 0 
-  print "\n"
-else
-  date += 1
+  if (date + first_wday) % 7 == 0 
+    print "\n"
+  else
+    date += 1
+  end
 end
-end
+
+
+
 
 
 # date = Date.new(2024, 06, 21)
