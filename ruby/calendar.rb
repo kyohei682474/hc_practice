939d12c0be68f4fd40d653f3bede7bc7c4efd501
kiyo <18kyohei@gmail.com>
2024-06-27 10:33:23 +0900

    remake code to use method

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,55 +1,76 @@
-require 'optparse'
-require 'date'
+# require  'date'
+# require 'optparse'
 
-# コマンドの解析
+# def calender(year, mon)
+#   header_top = "#{mon}月 #{year}" 
+#   first_wday = Date.new(year, mon, 1).wday
+#   last_date = Date.new(year, mon , -1).day
+
+#   puts header_top.center(20)
+#   puts "日 月 火 水 木 金 土"
+#   print " " * (first_wday * 3)
+#   (1..last_date).each do |date|
+#     print date.to_s.rjust(2) + " "
+#     if (date + first_wday) % 7 == 0
+#       print "\n"
+#     else
+#       date + 1
+#     end
+#   end
+# end
+
+# opt = OptionParser.new
+# options = {}
+# opt.on('-m mon', Integer) { |mon| options[:m] = mon }
+# opt.parse!(ARGV)
+
+# now = Date.today
+# year = Date.today.year
+
+#  # オプションをカレンダーに組み込む
+#  mon = options[:m] || now.month 
+#  if mon < 1 || 12 < mon
+#   puts "#{mon} is neither a month number (1..12) nor a name" 
+#   exit
+#  end
+
+#  calender(year, mon)
+
+require 'date'
+require 'optparse'
+
+def calender(year, mon)
+  header_top = "#{mon}月 #{year}" 
+  first_wday = Date.new(year, mon, 1).wday
+  last_date = Date.new(year, mon, -1).day
+
+  puts header_top.center(20)
+  puts "日 月 火 水 木 金 土"
+  print " " * (first_wday * 3)
+  (1..last_date).each do |date|
+    print "#{date.to_s.rjust(2)} "
+    # if (date + first_wday) % 7 == 0 
+    if ((date + first_wday) % 7).zero?
+      print "\n"
+    else
+      date + 1
+    end
+  end
+end
+
 opt = OptionParser.new
 options = {}
-opt.on('-m [mon]', Integer) { |mon| options[:m] = mon }
+opt.on('-m mon', Integer) { |mon| options[:m] = mon }
 opt.parse!(ARGV)
-p ARGV
 
 now = Date.today
 year = Date.today.year
 
+ # オプションをカレンダーに組み込む
 mon = options[:m] || now.month
-
-if mon < 1 || 12 < mon
+if mon < 1 || 12 < mon # rubocop:disable Style/YodaCondition
   puts "#{mon} is neither a month number (1..12) nor a name"
+  exit
 end
 
-
-weeks = ["日","月","火","水","木","金","土",]
-# %m: 月を表す数字(01-12)　%Y: 西暦を表す数
-top = Date.today.strftime("%m, %Y")
-# year = Date.today.year
-# mon = Date.today.month
-# 初日が何曜日かを把握
-first_wday = Date.new(year, mon, 1).wday
-# 最終日が何日か把握
-last_date = Date.new(year, mon, -1).day
-
-puts top.center(20)
-puts weeks.join(' ')
-print " " * (first_wday * 3)
-(1..last_date).each do |date|
-  print date.to_s.rjust(2) + " " 
-  if (date + first_wday) % 7 == 0 
-    print "\n"
-  else
-    date += 1
-  end
-end
-
-
-
-
-
-# date = Date.new(2024, 06, 21)
-# p date
-# opt = OptionParser.new
-
-# opt.on('-m VAL') {|v| p v }
-# opt.on('-b') {|v| p v }
-
-# opt.parse!(ARGV)
-# p ARGV
+calender(year, mon) # rubocop:disable Layout/TrailingEmptyLines
