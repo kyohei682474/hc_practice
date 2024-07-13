db9ba8af9d72246071c67f5677eadd28ccfdb366
kiyo <18kyohei@gmail.com>
2024-07-13 17:16:18 +0900

    add conditions of stock

--- ruby/vending_machine/vending_machine.rb
+++ ruby/vending_machine/vending_machine.rb
@@ -1,21 +1,23 @@
 class VendingMachine
   attr_accessor :stock
 #  デフォルト値としてpepsiの在庫を５本とする 
-  def initialize(stock = {'pepsi' => 5})
+  def initialize(stock = {'pepsi' => 1})
     @stock = stock
+    @sales_amount = 0
+
   end
 
   
 # ストックがある場合と全くない場合の補充の条件
   def add_drink(drink, quantity)
-    if @stock.key?(drink.kind)
+    if @stock.key?(drink.kind) 
       @stock[drink.kind] += quantity 
     else
       @stock[drink.kind] = quantity
     end
   end
   # ジュースが注文されストックが消費された時の条件。さらにsuicaの残高も減る。
-  def perchase_drink(drink, suica)
+  def purchase_drink(drink, suica)
     if @stock.key?(drink.kind) && @stock[drink.kind]> 0 && suica.money >= drink.price
        @stock[drink.kind] -= 1
        suica.money -= drink.price
@@ -28,24 +30,20 @@ class VendingMachine
       #   raise puts "お金が足りません"
       # end
 
-      exception_handling_of_perchase_drink
+      exception_handling_of_purchase_drink(drink, suica)
     end
     
   end
 
   # 購入における例外処理用のメソッド
-  def exception_handling_of_perchase_drink 
-    if @stock.key?(drink.kind) && @stock[drink.kind] = 0 
+  def exception_handling_of_purchase_drink(drink, suica)
+    if @stock.key?(drink.kind) && @stock[drink.kind] == 0 
       raise puts "在庫がありません"
     else @stock.key?(drink.kind) && @stock[drink.kind]> 0 && suica.money < drink.price
-      raise puts "お金が足りません"
+      raise 
     end
   end
-
-  def default_stock_of_drink(drink)
-    @stock[drink.kind] || 0
-  end
-
+ 
   # 売上金額の更新
   def update_sales_amount(amount)
     # 売上金額を格納する
@@ -103,7 +101,7 @@ class Suica
 pepsi = Drink.new('pepsi',150)
 vending_machine = VendingMachine.new
 vending_machine.stock
-
+p vending_machine.stock
 suica = Suica.new
 # 例外を発生させる
 begin
@@ -112,6 +110,6 @@ rescue RuntimeError => e
   puts "チャージ金額が100円未満のためにエラーが生じています。"
   puts "エラー:#{e.class} #{e.message}"
 end
-
-
-
+vending_machine.purchase_drink(pepsi,suica)
+p vending_machine.stock
+irohasu = Drink,
