b4c98009bfcb8fe5451a7e2a2eb75e40748d4569
kiyo <18kyohei@gmail.com>
2024-07-28 20:54:56 +0900

    write module

--- ruby/pokemon.rb
+++ ruby/pokemon.rb
@@ -1,6 +1,21 @@
-class Pokemon
-  attr_reader :name, :type1, :type2, :hp
+module NameService
+  def name_change(newname)
+    if newname == 'うんこ'
+      puts "不適切な名前です。"
+    else
+      @name = newname
+    end
+  end
 
+  def get_name 
+    name
+  end
+
+end
+
+class Pokemon
+  include NameService
+  attr_reader :name, :type1, :type2, :hp
   def initialize(name, type1, type2, hp)
     @name = name
     @type1 = type1
@@ -11,19 +26,34 @@ class Pokemon
   def attack
     puts "#{@name}の攻撃"
   end
+
+
+
+  private 
+  def name=(value)
+    @name = value
+  end
+
 end
 
-class Pikachu < Pokemon # rubocop:disable Style/Documentation
+class Pikachu < Pokemon # rubocop:disable Style/Documentation 
+
   def initialize(name, type1, type2, hp)
     super
+  end
 
     def attack
       super
-      puts '10万ボルト'
+      puts "#{get_name}の10万ボルト"
     end
-  end
+  
+end
+
+class Player
+  include NameService
 end
 
 poke = Pikachu.new('ピカチュウ', 'でんき', '_', 100)
 p poke.name
+
 poke.attack
