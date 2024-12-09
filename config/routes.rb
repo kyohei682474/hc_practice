aeef367ef46c35a5182f2da61a050878cb4960d1
kiyo <18kyohei@gmail.com>
2024-12-09 10:17:19 +0900

    create controller of items

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'items#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
