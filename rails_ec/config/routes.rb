bcc5c3fd8553f4e953c171d0b7fcd4f1ea97b031
kiyo <18kyohei@gmail.com>
2024-12-25 13:13:11 +0900

    change item.index

--- rails_ec/config/routes.rb
+++ rails_ec/config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   
   get 'items/show'
   resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
   root "items#index"
